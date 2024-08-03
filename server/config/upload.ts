569fe1289b2d7d18b8e164d21978cfa19796dcdf
Omer Gaiznger <Omerg863@gmail.com>
2024-08-03 13:58:10 +0300

    multer storage

--- server/config/upload.ts
+++ server/config/upload.ts
@@ -1,10 +1,11 @@
 import multer from 'multer';
 import fs from 'fs';
 import { promisify } from 'util';
+const storage = multer.memoryStorage();
 
 const unlinkAsync = promisify(fs.unlink);
 
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({ storage });
 
 
 export { upload, unlinkAsync };
