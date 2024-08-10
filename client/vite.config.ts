be588dd490a045efe4221cae94593771ca22ba3a
Omer Gaiznger <Omerg863@gmail.com>
2024-08-10 19:11:10 +0300

    url handler

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,34 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { VitePWA } from "vite-plugin-pwa";
-
-const manifestForPlugIn = {
-  manifest:{
-    name:"Super Cart",
-    short_name:"Super Cart",
-    description:"Super Cart - Super Fast Shopping",
-    icons: [
-      {
-        "src": "icon192.png",
-        "sizes": "192x192",
-        "type": "image/png",
-        "purpose": "any maskable"
-      },
-      {
-        "src": "icon512.png",
-        "sizes": "512x512",
-        "type": "image/png",
-        "purpose": "any maskable"
-      }
-    ],
-  theme_color:'#213547',
-  background_color:'#213547',
-  scope:'/',
-  start_url:"/"
-  }
-}
+import manifest from "./manifest.json";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA(manifestForPlugIn)],
+  plugins: [react(), VitePWA(manifest)],
 })
